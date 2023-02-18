5a2c38f073e2dce9c70f552fb5bd2103f309748a
Tambu Precious <86779545+Boyprecious@users.noreply.github.com>
2023-02-18 06:40:50 +0100

    Add files via upload
    Simple Stack in C++ 
    The MAX enum variable can be change to a const unsigned int 
    then push{add item}
    or pop{veiw item}
    
    notice the 
    ++item in push
    this is so that , when you push,  it should increment before adding the new item to a new position. 
    the item-- 
    in pop will show the item before moving to  a lower location. 
    
    Lucky luck

---
+++ 12_firstStack.cpp
@@ -0,0 +1,74 @@
+/*Author[0] : Tambu Precious @ BoyPrecious
+Aim: push and pop from a stack.
+*/
+
+# include <iostream>
+
+using namespace std;
+
+class Stack{
+
+private:    // all of these guys private, so that user can't edit
+    enum {MAX=10};
+    int stackArray[MAX];
+    int top;
+public:
+    Stack():top(0)
+    {/*nothing*/}
+
+    void push(int var){
+        if(top>=xi10){
+            cout<<"Full"<<endl;
+        }
+        else{
+        stackArray[++top] = var;
+        }
+    }
+
+    int pop(){
+        if(top<0){
+            cout<<"end"<<endl;
+            return (stackArray[0]);
+        }
+
+        else{
+        return stackArray[top--];
+        }
+
+    }
+};
+
+
+
+int main(){
+
+    Stack s1;
+    char popPush;
+
+    cout<<"push/pop/exit?:(p, P, e):  ";
+    cin>>popPush;
+
+    while(popPush!='e'){
+
+        switch(popPush){
+
+            case 'P':
+                int temp;
+                cout<<"INPUT INT: "; cin>>temp; cout<<"Done!"<<endl;
+                s1.push(temp);
+                break;
+
+            case 'p':
+                cout<<"data: "<<s1.pop()<<endl;
+                break;
+        }
+
+
+        cout<<"push/pop/exit?:(p, P, e):  ";
+        cin>>popPush;
+
+    }
+
+ cout<<"Exited"<<endl;
+return 0;
+}
