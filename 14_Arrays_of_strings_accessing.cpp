27378808d553dc67d12ab254e663275c5d7a47b2
Tambu Precious <86779545+Boyprecious@users.noreply.github.com>
2023-02-19 11:10:31 +0100

    Add files via upload
    No need to use double for loops to access  a two dimensional array ,  just like in python , array[k][:] gives everything in in that row so does c plus plus. Awesome right?

---
+++ 14_Arrays_of_strings_accessing.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    const int day = 7;
+    const int letters = 10;
+    char days[day][letters] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+    for(int i=0; i<7; i++){
+
+        cout<<days[i]<<endl;
+    }
+
+
+
+
+
+
+
+}
